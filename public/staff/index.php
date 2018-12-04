d6d8c997a68ee60a34cdb783125c7bc9239052d7
Gregg Christofferson <25208248+GREGG-CHRISTOFFERSON@users.noreply.github.com>
2018-12-03 23:08:52 -0800

    first commit

---
+++ public/staff/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <title>GBI</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+    </head>
+
+    <body>
+        <header>
+            <h1>GBI Staff Area</h1>
+
+        </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+            </ul>
+
+        </nav>
+        <div id="content">
+            
+        </div>
+        <footer>
+            &copy; <?php echo date('Y'); ?> Globe Bank
+
+        </footer>
+
+    </body>
+</html>

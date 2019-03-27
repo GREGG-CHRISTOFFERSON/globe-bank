59eb83ee188a24064f02ee8ce1489bb938bbff9f
Gregg Christofferson <gechristofferson@gmail.com>
2019-03-27 14:44:32 -0700

    added form validation for pages

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -19,14 +19,20 @@ if(is_post_request()) {
     $page['content'] = $_POST['content'] ?? '';
 
     $result = update_page($page);
-    redirect_to(url_for('/staff/pages/show.php?id=' . $id));
+    if ($result === true) {
+        redirect_to(url_for('/staff/pages/show.php?id=' . $id));
+    } else {
+      $errors = $result;
+    }
+
 } else {
   $page = find_page_by_id($id);
 
-  $page_set = find_all_pages();
-  $page_count = mysqli_num_rows($page_set);
-  mysqli_free_result($page_set);
 }
+
+$page_set = find_all_pages();
+$page_count = mysqli_num_rows($page_set);
+mysqli_free_result($page_set);
 
 ?>
 
@@ -39,6 +45,8 @@ if(is_post_request()) {
 
     <div class="subject edit">
         <h1>Edit Page</h1>
+
+        <?php echo display_errors($errors); ?>
 
         <form action="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($id))); ?>" method="post">
           <dl>
@@ -88,8 +96,14 @@ if(is_post_request()) {
                     } ?>/>
                 </dd>
             </dl>
+            <dl>
+                <dt>Content</dt>
+                <dd>
+                  <textarea name="content" cols="60" rows="10"><?= h($page['content']); ?></textarea>
+                </dd>
+            </dl>
             <div id="operations">
-                <input type="submit" value="Edit Subject" />
+                <input type="submit" value="Edit Page" />
             </div>
         </form>
 
